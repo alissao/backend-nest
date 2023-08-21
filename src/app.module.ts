41dfd94f44da3bc67b0d005a22f37b484c6f98b7
alissao <alissonhbv@gmail.com>
2023-08-21 17:34:32 -0300

    Added Places Service hitting google places API. Moved Google API Key to untracked json file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RoutesModule } from './routes/routes.module';
+import { ConfigModule } from '@nestjs/config';
+import { MapsModule } from './maps/maps.module';
+import config from './config/configuration';
 
 @Module({
-  imports: [PrismaModule, RoutesModule],
+  imports: [
+    ConfigModule.forRoot({ 
+      isGlobal: true ,
+      load: [config]
+    }),
+    PrismaModule, 
+    RoutesModule, 
+    MapsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
